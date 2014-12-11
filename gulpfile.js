0af0a46c2acb7594371b1fc3ab4f7879fcf1360e
Eirik S. Morland <eirik@morland.no>
2014-12-11 17:54:58 +0100

    Add current gulpfile and package.json.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,22 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var livereload = require('gulp-livereload');
+var fs = require('fs');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+gulp.task('scss', function() {
+  gulp.src('_sass/style.scss')
+    .pipe(sass({
+      outputStyle: 'compressed',
+      errLogToConsole: true,
+      error: function(err) {
+        console.log(err);
+      }
+    }))
+    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(gulp.dest('./css'));
+});
 
 gulp.task('scripts', function() {
   browserify('./js/main.js', {debug: true})
@@ -10,8 +26,20 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('build', function() {
+  var htmls = require('../brew-yml-to-html')('./brews.yml', fs.readFileSync('./templates/brew.html', 'utf-8'));
+  for (var prop in htmls) {
+    // Create a html file out of this.
+    var w = fs.createWriteStream(prop + '.html');
+    w.write(htmls[prop]);
+    w.end();
+  }
+});
+
 gulp.task('watch', function() {
   livereload.listen();
   gulp.watch('build/**').on('change', livereload.changed);
+  gulp.watch('css/**').on('change', livereload.changed);
   gulp.watch(['js/**'], ['scripts']);
+  gulp.watch(['_sass/**'], ['scss']);
 });
